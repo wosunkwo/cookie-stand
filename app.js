af0f9a1d37898f60626a6d37ccbfb638e040ef44
williams osunkwo <wosunkwo@gmail.com>
2019-04-03 13:19:10 -0700

    added linter to my java script

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var hours = ['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm','8:00pm'];
 var stores = ['1st and Pike', 'SeaTac Airport','Seattle Center','Capital Hill','Alki'];
 var minStoreCustomer = [23,3,11,20,2];
